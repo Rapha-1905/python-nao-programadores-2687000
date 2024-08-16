f01b0194fa00ae71325e5dea16fd94086f484b56
Rapha-1905 <raphael_lima.souza@daimlertruck.com>
2024-08-16 18:07:14 +0000

    Variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+profissão='Vendedora'
+cargo='gerente'
+nome="Raphael"
+
 
 # Declare variáveis de cada tipo numérico
-
+numero inteiro='6'
+float='6.6'
 
 # Declare uma variável do tipo booleano
+verdadeiro='true'
+falso='false'
